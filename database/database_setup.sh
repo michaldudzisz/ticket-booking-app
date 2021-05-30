9518510294db3c0a85a0d63fe64785dac17b2fcb
michaldudzisz <49537887+michaldudzisz@users.noreply.github.com>
2021-05-30 19:15:03 +0200

    Update database_setup.sh
    Fixing bug with pasting shell variable

--- database/database_setup.sh
+++ database/database_setup.sh
@@ -1,11 +1,10 @@
-# This is path to maven repository location default on Ubuntu Linux.
+# The location after "cp" flag is path to maven repository location default on Ubuntu Linux.
 # Please change it, if it is different on your machine.
-MAVEN_REPOSITORY_PATH="~/.m2/repository"
 
-java -cp $MAVEN_REPOSITORY_PATH/com/h2database/h2/1.4.200/h2-1.4.200.jar org.h2.tools.RunScript \
+java -cp ~/.m2/repository/com/h2database/h2/1.4.200/h2-1.4.200.jar org.h2.tools.RunScript \
 -user sa -password password -url jdbc:h2:./cinema -script ./schema.sql
 
-java -cp $MAVEN_REPOSITORY_PATH/com/h2database/h2/1.4.200/h2-1.4.200.jar org.h2.tools.RunScript \
+java -cp ~/.m2/repository/com/h2database/h2/1.4.200/h2-1.4.200.jar org.h2.tools.RunScript \
 -user sa -password password -url jdbc:h2:./cinema -script ./data.sql
 
 
