9462b0579eb64cd6bd768bc8e9c040151d6afda1
michaldudzisz <49537887+michaldudzisz@users.noreply.github.com>
2021-05-30 19:16:43 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 # you should have maven installed. In case you don't have, run <sudo apt-get install maven>
-# build app:
-mvn clean install
+# resolve dependencies, build app:
+mvn compile
 
 # prepare database:
 cd ./database
 sh ./database_setup.sh
 cd ..
+
+# create executable:
+mvn clean install
